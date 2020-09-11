7222b805182d24da8d1f2ca1bd410bc8a88b8697
骆明 <736704198@qq.com>
2020-09-11 17:36:48 +0800

    TCP 三次握手与四次挥手

--- 03 简单的web服务器实现/01 TCP3次握手、4次挥手.py
+++ 03 简单的web服务器实现/01 TCP3次握手、4次挥手.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 # -*- coding:utf-8 -*- 
 # author: Ming Luo
-# time: 2020/9/10 17:34
+# time: 2020/9/10 17:36
 
 # 需求：
 # 利用socket建立服务器，并且能够利用浏览器进行访问
@@ -14,8 +14,6 @@ import threading
 def work(client_socket, client_addr):
     while True:
         # 接收对方发送过来的数据
-        print(client_socket)
-        print(client_addr)
         recv_data = client_socket.recv(1024).decode("gbk")  # 接收1024个字节
         # 如果recv解堵塞，那么有2种方式:
         # 1. 客户端发送过来数据
@@ -24,17 +22,22 @@ def work(client_socket, client_addr):
             break
         print('接收到的数据为:\n', recv_data)
 
-        return_data = "HTTP/1.1 200 OK\n\n<h1>你好!</h1>"
+        response = "HTTP/1.1 200 OK\r\n"
+        response += "\r\n"
+        body = "<h1>你好!</h1>\r\n"
+        return_data = response + body
         # 发送一些数据到客户端
-        client_socket.send(return_data.encode('gbk'))
+        client_socket.send(return_data.encode('utf-8'))
     print('---- 客户%s服务完毕 ----' % str(client_addr))
     # 关闭为这个客户端服务的套接字,只要关闭了，就意味着不能再为这个客户端服务了，如果还需要服务，只能再次访问
     client_socket.close()
 
+
 def main():
     # 创建socket
     tcp_server_socket = socket(AF_INET, SOCK_STREAM)
-
+    # 设置当服务器先close 即服务器端4次握手之后资源能够立即释放，这样就保证了，下次运行程序时，可以立即使用该端口
+    tcp_server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     # 本地信息
     address = ('127.0.0.1', 8070)
 
@@ -56,3 +59,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
