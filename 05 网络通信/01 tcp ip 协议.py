b2d225c4cec8d5e6c8c2d0be745d4906dc2da6de
骆明 <736704198@qq.com>
2020-09-14 15:29:46 +0800

    网络通信

--- 05 网络通信/01 tcp ip 协议.py
+++ 05 网络通信/01 tcp ip 协议.py
@@ -1,7 +1,11 @@
 # __author__:"Ming Luo"
 # date:2020/9/13
 
-
+# TCP/IP（Transmission Control Protocol/Internet Protocol，
+# 传输控制协议/网际协议）是指能够在多个不同网络间实现信息传输的协议簇。
+# TCP/IP协议不仅仅指的是TCP 和IP两个协议，而是指一个由FTP、SMTP、TCP、UDP、IP等协议构成的协议簇，
+# 只是因为在TCP/IP协议中TCP协议和IP协议最具代表性，所以被称为TCP/IP协议。
+# TCP/IP 协议簇
 
 
 
